3a11d89b4be62c714b26bc00a7e97493a71645e1
oppsec <daniel.sdm.github@gmail.com>
2021-06-22 21:37:00 -0300

    release: Apekar 0.2a

--- src/core/main.rb
+++ src/core/main.rb
@@ -1,6 +1,8 @@
 require 'colorize'
 require 'json'
 
+require_relative 'helper.rb'
+
 
 # Get XML file name
 def initializer()
@@ -8,23 +10,18 @@ def initializer()
     print(">> XML File Name: ".red)
 
     file_name = gets.chomp
-
     xml_content = File.open(file_name).readlines
 
-    sensitive_analyze(file_name, xml_content)
-
+    sensitive_analyze(xml_content)
 end
 
 
-# Search for API keys, domains etc... based on keys.json file
-def sensitive_analyze(file_name, xml_content)
-    puts "\n>> Checking for sensitive content in: #{file_name}".cyan
-
-    keys_file = File.read('./src/core/data/keys.json')
-    keys_json = JSON.parse(keys_file)
+# Search for API keys, domains etc... (keys.json)
+def sensitive_analyze(xml_content)
+    puts $msg[:keys].cyan
 
     xml_content.each do |line|
-        keys_json.each do |key, value|
+        extract_data("keys").each do |key, value|
 
             if line =~ /#{value}/
                 puts ">> #{key} found: #{line.strip()}".green
@@ -33,19 +30,31 @@ def sensitive_analyze(file_name, xml_content)
         end
     end
 
-    permissions_analyze(file_name, xml_content)
+    permissions_analyze(xml_content)
 end
 
 
-# Check for app required permissions... based on permissions.json file
-def permissions_analyze(file_name, xml_content)
-    puts "\n>> Getting app required permissions".cyan
-
-    permissions_file = File.read('./src/core/data/permissions.json')
-    permissions_json = JSON.parse(permissions_file)
+# Check for app required permissions... (permissions.json)
+def permissions_analyze(xml_content)
+    puts $msg[:perms].cyan
 
     xml_content.each do |line|
-        permissions_json.each do |key, value|
+        extract_data("permissions").each do |key, value|
+            if line =~ /#{value}/
+                puts ">> #{key}".green
+            end
+        end
+    end
+
+    services_analyze(xml_content)
+end
+
+# Check for third party services... (services.json)
+def services_analyze(xml_content)
+    puts $msg[:serv].green
+
+    xml_content.each do |line|
+        extract_data("services").each do |key, value|
 
             if line =~ /#{value}/
                 puts ">> #{key}".green
